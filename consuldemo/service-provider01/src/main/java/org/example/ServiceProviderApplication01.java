79ab21f62ce62471c399d662962111da4956e1a7
pengtao <3548297839@qq.com>
2023-09-16 10:39:17 +0800

    consuldemo父工程

---
+++ consuldemo/service-provider01/src/main/java/org/example/ServiceProviderApplication01.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServiceProviderApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
