9ea227e414fb40c6e88e63edc341bc82453d07aa
pengtao <3548297839@qq.com>
2023-09-16 10:53:00 +0800

    eureka-serverer01启动类

--- eurekademo/eureka-server01/src/main/java/org/example/EurekaServer01Application.java
+++ eurekademo/eureka-server01/src/main/java/org/example/EurekaServer01Application.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer01Application.class);
+
     }
 }
