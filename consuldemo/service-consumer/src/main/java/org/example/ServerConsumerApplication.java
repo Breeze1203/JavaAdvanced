4ef8bc4231c51e81fe4a3ca7a4d66fa739fcad8d
pengtao <3548297839@qq.com>
2023-09-16 10:41:37 +0800

    consuld-consumer消费者

--- consuldemo/service-consumer/src/main/java/org/example/ServerConsumerApplication.java
+++ consuldemo/service-consumer/src/main/java/org/example/ServerConsumerApplication.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class ServerConsumerApplication
 {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(ServerConsumerApplication.class);
     }
 }
