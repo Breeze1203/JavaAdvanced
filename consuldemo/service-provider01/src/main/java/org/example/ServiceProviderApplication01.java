6165f738d60d0674baf4153e7d1818a838caae40
pengtao <3548297839@qq.com>
2023-09-16 10:42:29 +0800

    consuld-provider01提供者启动类

--- consuldemo/service-provider01/src/main/java/org/example/ServiceProviderApplication01.java
+++ consuldemo/service-provider01/src/main/java/org/example/ServiceProviderApplication01.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
- *
  */
-public class ServiceProviderApplication
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class ServiceProviderApplication01 {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderApplication01.class);
     }
 }
