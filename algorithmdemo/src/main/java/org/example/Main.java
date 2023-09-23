cf3b370cdd058461cb9e7aae8a1473b8ca944aeb
pengtao <3548297839@qq.com>
2023-09-23 18:55:35 +0800

    删除数组的重复项26

--- algorithmdemo/src/main/java/org/example/Main.java
+++ algorithmdemo/src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        boolean palindrome = isPalindrome(1212);
-        System.out.println(palindrome);
+        //boolean palindrome = isPalindrome(1212);
+        //System.out.println(palindrome);
+//     罗马数字测试
+
     }
 
     public static boolean isPalindrome(int x) {
