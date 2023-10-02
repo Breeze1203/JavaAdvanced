6e8554419ac435c7c1f49d823afad82dac00b22e
pengtao <3548297839@qq.com>
2023-10-02 19:27:48 +0800

    modified

--- eurekademo/eureka-provider01/src/main/java/org/example/EurekaProvider01Application.java
+++ eurekademo/eureka-provider01/src/main/java/org/example/EurekaProvider01Application.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class EurekaProvider01Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(EurekaProvider01Application.class);
     }
 }
