a3eb3e6bd1afa8fb46e20acd43bdba4261259fd1
pengtao <3548297839@qq.com>
2023-10-02 19:27:20 +0800

    leetcode

--- algorithmdemo/src/main/java/org/example/leetcode/strStr28.java
+++ algorithmdemo/src/main/java/org/example/leetcode/strStr28.java
@@ -1,2 +1,23 @@
-package org.example.leetcode;public class strStr28 {
+package org.example.leetcode;
+
+public class strStr28 {
+    public static int strStr(String haystack, String sta) {
+        int i=0;
+        String a=haystack;
+        while(true){
+            if(!a.startsWith(sta)){
+                a=a.substring(i+1,a.length());
+                i++;
+            }else {
+                break;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int i = strStr("haystack", "sta");
+        System.out.println(i);
+
+    }
 }
