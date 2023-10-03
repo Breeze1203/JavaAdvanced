fb6ecd25b52b94b794e9df56612d2defcfa52318
pengtao <3548297839@qq.com>
2023-10-03 20:12:11 +0800

    modified

--- springcloud/eureka-provider/src/main/java/org/example/EurekaProviderApplication.java
+++ springcloud/eureka-provider/src/main/java/org/example/EurekaProviderApplication.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
