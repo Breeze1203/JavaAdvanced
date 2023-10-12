6013d0a4d94a3b2244c3c4654745ec2e7d6a81be
pengtao <3548297839@qq.com>
2023-10-12 08:21:42 +0800

    rbac权限模块修改

--- rbacdemo/src/main/java/com/example/rbacdemo/controller/RbacController.java
+++ rbacdemo/src/main/java/com/example/rbacdemo/controller/RbacController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rbac")
 public class RbacController {
 
     @GetMapping("/login")
