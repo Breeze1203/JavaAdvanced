dc3026e1ebb3ee06ded8c84ad7a72c6a00b2804b
pengtao <3548297839@qq.com>
2023-10-31 20:58:54 +0800

    添加权限认证的拦截器

--- adminflow/src/main/java/com/example/adminflow/interceptor/CheckPermissionInterceptor.java
+++ adminflow/src/main/java/com/example/adminflow/interceptor/CheckPermissionInterceptor.java
@@ -1,10 +1,14 @@
 package com.example.adminflow.interceptor;
 
+import com.example.adminflow.permission.CheckPermission;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Method;
 
 @Component
 public class CheckPermissionInterceptor implements HandlerInterceptor {
@@ -12,16 +16,15 @@ public class CheckPermissionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 在请求处理之前进行拦截逻辑的编写 返回true表示继续执行请求，返回false表示拦截请求
-//        if (handler instanceof HandlerMethod handMethod) {
-//            // 获取方法对象
-//            Method method = handMethod.getMethod();
-//            // 获取方法对象上的注解对象
-//            CheckPermission annotation = method.getAnnotation(CheckPermission.class);
-//            if (annotation != null) {
-//                String permission = annotation.permission();
-//                //测试数据
-//                List<String> userPermissionList = new ArrayList<>();
-//        }
+        if (handler instanceof HandlerMethod handMethod) {
+            // 获取方法对象
+            Method method = handMethod.getMethod();
+            // 获取方法对象上的注解对象
+            CheckPermission annotation = method.getAnnotation(CheckPermission.class);
+            if (annotation != null) {
+                String[] permission = annotation.permission();
+            }
+        }
         return true;
     }
 
