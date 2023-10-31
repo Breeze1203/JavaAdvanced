cc4fb6853dff195feeb99435aabeb1d25bc55c8d
pengtao <3548297839@qq.com>
2023-10-31 20:50:34 +0800

    aop记录用户登录登出日志

--- adminflow/src/main/java/com/example/adminflow/config/AopConfig.java
+++ adminflow/src/main/java/com/example/adminflow/config/AopConfig.java
@@ -1,8 +1,8 @@
 package com.example.adminflow.config;
 
 
-import com.example.adminflow.model.LoginData;
-import com.example.adminflow.service.LoginDataService;
+import com.example.adminflow.model.OperationData;
+import com.example.adminflow.service.OperationDataService;
 import com.example.adminflow.util.DateUtil;
 import com.example.adminflow.util.IpUtil;
 import com.example.adminflow.util.StatusUtil;
@@ -14,12 +14,15 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+/*
+该类做日志处理
+ */
 
 @Aspect
 @Component
 public class AopConfig {
-    @Resource(name = "LogDataService")
-    LoginDataService loginDataService;
+    @Resource(name = "OperationDataService")
+    OperationDataService operationDataService;
 
     @AfterReturning(pointcut = "execution(* com.example.adminflow.controller.UserController.login(..))",returning = "result")
     public void afterLoginSuccess(JoinPoint joinPoint, Object result){
@@ -28,18 +31,34 @@ public class AopConfig {
         // 这个是获取方法的参数
         Object[] args = joinPoint.getArgs();
         if(status.getMessage().equals("用户名或密码错误")) return;
-        LoginData loginData = new LoginData();
+        OperationData operationData = new OperationData();
         String u=args[0].toString();
-        loginData.setLogin_user(u);
-        loginData.setLogin_Date(DateUtil.formatLog(new Date()));
+        operationData.setUser(u);
+        operationData.setDate(DateUtil.formatLog(new Date()));
         for (Object arg : args) {
             if(arg instanceof HttpServletRequest){
                 String ipAddr = IpUtil.getIpAddr((HttpServletRequest) arg);
-                loginData.setOperation_log(u+"用户登录;ip地址为"+ipAddr);
+                operationData.setType("用户登录");
+                operationData.setOperation(u+"用户登录;ip地址为"+ipAddr);
                 break;
             }
         }
-        loginDataService.insetLoginLog(loginData);
+        operationDataService.insetOperationLog(operationData);
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.adminflow.controller.UserController.loginOut(..))",returning = "result")
+    public void afterLoginOutSuccess(JoinPoint joinPoint, Object result){
+        StatusUtil status = (StatusUtil) result;
+        // 这个是获取方法的参数
+        Object[] args = joinPoint.getArgs();
+        if(status.getCode()==500) return;
+        OperationData operationData = new OperationData();
+        operationData.setType("退出登录");
+        String string = args[0].toString();
+        operationData.setUser(string);
+        operationData.setDate(DateUtil.formatLog(new Date()));
+        operationData.setOperation(string+"退出登录");
+        operationDataService.insetOperationLog(operationData);
     }
 
 }
