468e3ab8cdaa36606f1ff49cd3d51e50bc2acf16
pengtao <3548297839@qq.com>
2023-12-09 21:09:13 +0800

    用户私信模块

---
+++ admin/src/main/java/com/example/admin/controller/MessageController.java
@@ -0,0 +1,51 @@
+package com.example.admin.controller;
+
+import com.example.admin.model.Message;
+import com.example.admin.service.MessageService;
+import com.example.admin.util.DateUtil;
+import com.example.admin.util.StatusUtil;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class MessageController {
+    @Resource(name = "MessageService")
+    private MessageService messageService;
+
+    @GetMapping("/messageInit")
+    public List<Message> getMessage(@RequestParam("mid")Integer id){
+        return messageService.getAllMessage(id);
+    }
+
+    @PostMapping("/sendMessage")
+    public StatusUtil saveMessage(@RequestParam("sid")Integer sid,@RequestParam("rid")Integer rid,@RequestParam("content")String content){
+        Message message = new Message();
+        message.setContent(content);
+        message.setState(false);
+        message.setSend_id(sid);
+        message.setReceive_id(rid);
+        message.setTime(DateUtil.format(new Date()));
+        Integer i = messageService.sendMessage(message);
+        if(i>0){
+            return new StatusUtil("发送成功",200,null);
+        }else {
+            return new StatusUtil("网络出现异常,请稍后再试",500,null);
+        }
+    }
+
+    @GetMapping("/updateState")
+    public StatusUtil upState(@RequestParam("id")Integer id){
+        Integer i = messageService.upState(id);
+        if(i>0){
+            return new StatusUtil("修改成功",200,null);
+        }else {
+            return new StatusUtil("网络出现异常,请稍后再试",500,null);
+        }
+    }
+}
