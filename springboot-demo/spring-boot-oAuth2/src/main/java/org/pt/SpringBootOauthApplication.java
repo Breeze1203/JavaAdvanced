936c2f07adfae4d746699f31bac8b9fb3d3fbb51
pengtao <3548297839@qq.com>
2024-04-27 22:16:39 +0800

    OAutn2 实战

---
+++ springboot-demo/spring-boot-oAuth2/src/main/java/org/pt/SpringBootOauthApplication.java
@@ -0,0 +1,11 @@
+package org.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauthApplication.class);
+    }
+}
