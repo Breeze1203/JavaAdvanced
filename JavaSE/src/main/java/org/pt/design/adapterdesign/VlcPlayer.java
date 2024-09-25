b2ecc133cac5a688bff4555d2fd729d7f89491a8
pengtao <3548297839@qq.com>
2024-09-25 15:55:15 +0800

    :sparkles: 模版模式

---
+++ JavaSE/src/main/java/org/pt/design/adapterdesign/VlcPlayer.java
@@ -0,0 +1,15 @@
+package org.pt.design.adapterdesign;
+/*
+接口实现类2
+ */
+public class VlcPlayer implements AdvancedMediaPlayer{
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: "+ fileName);
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+
+    }
+}
---
+++ JavaSE/src/main/java/org/pt/design/templatedesign/FootBall.java
@@ -0,0 +1,18 @@
+package org.pt.design.templatedesign;
+
+public class FootBall extends Game{
+    @Override
+    protected void initialize() {
+        System.out.println("FootBall initialize.....");
+    }
+
+    @Override
+    protected void startPlay() {
+        System.out.println("FootBall start.....");
+    }
+
+    @Override
+    protected void endPlay() {
+        System.out.println("FootBall end.....");
+    }
+}
